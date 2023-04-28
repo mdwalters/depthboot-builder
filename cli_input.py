426893d56cf7932e35467fc94c2f0d5824f72a07
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-04-28 22:11:25 +0200

    Discourage use of ubuntu 22.04

--- cli_input.py
+++ cli_input.py
@@ -46,8 +46,8 @@ def get_user_input(verbose_kernel: bool, skip_device: bool = False) -> dict:
                 output_dict["distro_name"] = "ubuntu"
                 output_dict["distro_version"] = ia_selection("Which Ubuntu version would you like to use?",
                                                              options=["22.04", "23.04"], flags=[
-                        f"{os_sizes['ubuntu_22.04']['cli']}GB (LTS version)",
-                        f"{os_sizes['ubuntu_23.04']['cli']}GB (latest)"])
+                        f"{os_sizes['ubuntu_23.04']['cli']}GB (latest, recommended)",
+                        f"{os_sizes['ubuntu_22.04']['cli']}GB (LTS version)"])
                 break
             case "Linux Mint (Cinnamon edition)":
                 output_dict["distro_name"] = "ubuntu"
