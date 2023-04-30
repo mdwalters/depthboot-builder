5df6c7e64e2d21819880d001a89eefe833b2f180
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-04-30 23:04:22 +0200

    Fix popos stuck on upgrading
    A file in a package was updated in the remote repo, but the iso doesnt have it. This causes apt to stop and ask the user on what should be done -> just copy over the new file before running apt upgrade

--- distro/pop_os.py
+++ distro/pop_os.py
@@ -6,7 +6,8 @@ def config(de_name: str, distro_version: str, verbose: bool, kernel_version: str
     set_verbose(verbose)
     print_status("Configuring Pop!_OS")
 
-    print_status("Removing casper debs")  # Source : https://github.com/pop-os/distinst
+    print_status("Removing casper debs")
+    # List of packages to remove was taken from /cdrom/casper/filesystem.manifest-remove from the iso
     chroot("apt-get purge -y btrfs-progs casper cifs-utils distinst distinst-v2 dmraid expect f2fs-tools fatresize "
            "gettext gparted gparted-common grub-common grub2-common kpartx kpartx-boot libdistinst libdmraid1.0.0.rc16"
            " libinih1 libnss-mymachines localechooser-data os-prober pop-installer pop-installer-casper pop-shop-casper"
@@ -20,8 +21,14 @@ def config(de_name: str, distro_version: str, verbose: bool, kernel_version: str
         file.write("deb [signed-by=/usr/local/share/keyrings/eupnea.key] https://eupnea-linux.github.io/"
                    "apt-repo/debian_ubuntu jammy main")
     # update apt
+    print_status("Updating and upgrading all packages")
     chroot("apt-get update -y")
+    # TODO: Remove this once the iso is updated
+    # This file was updated in the remote package, but not on the iso. This results in apt prompting on what to do
+    # -> just copy over the new file from the package
+    cpfile("configs/pop-os/20apt-esm-hook.conf", "/mnt/depthboot/etc/apt/apt.conf.d/20apt-esm-hook.conf")
     chroot("apt-get upgrade -y")
+    print_status("Installing eupnea packages")
     # Install eupnea packages
     chroot("apt-get install -y eupnea-utils eupnea-system keyd")
 
