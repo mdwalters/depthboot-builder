2ea8db62c246593c997d2ed72357fdb79e627193
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-05-07 20:05:49 +0200

    Add rootfs verification after download

--- build.py
+++ build.py
@@ -50,11 +50,22 @@ def download_rootfs(distro_name: str, distro_version: str) -> None:
                 print_status("Downloading latest arch rootfs from geo.mirror.pkgbuild.com")
                 download_file("https://geo.mirror.pkgbuild.com/iso/latest/archlinux-bootstrap-x86_64.tar.gz",
                               "/tmp/depthboot-build/arch-rootfs.tar.gz")
+                download_file("https://geo.mirror.pkgbuild.com/iso/latest/sha256sums.txt",
+                              "/tmp/depthboot-build/checksum.sha256")
+                # rename archlinux-bootstrap-x86_64.tar.gz to arch-rootfs.tar.gz in checksum file
+                with open("/tmp/depthboot-build/checksum.sha256", "r") as checksum_file:
+                    checksum_og = checksum_file.read()
+                with open("/tmp/depthboot-build/checksum.sha256", "w") as checksum_file:
+                    checksum_file.write(checksum_og.replace("archlinux-bootstrap-x86_64.tar.gz", "arch-rootfs.tar.gz"))
             case "ubuntu" | "fedora":
                 print_status(f"Downloading {distro_name} rootfs, version {distro_version} from eupnea github releases")
                 download_file(f"https://github.com/eupnea-linux/{distro_name}-rootfs/releases/latest/download/"
                               f"{distro_name}-rootfs-{distro_version}.tar.xz",
                               f"/tmp/depthboot-build/{distro_name}-rootfs.tar.xz")
+                print_status("Downloading sha256sum for rootfs")
+                download_file(f"https://github.com/eupnea-linux/{distro_name}-rootfs/releases/latest/download/"
+                              f"{distro_name}-rootfs-{distro_version}.sha256",
+                              f"/tmp/depthboot-build/checksum.sha256")
             case "pop-os":
                 print_status("Downloading pop-os rootfs from eupnea github releases")
                 download_file("https://github.com/eupnea-linux/pop-os-rootfs/releases/latest/download/pop-os-rootfs-"
@@ -62,12 +73,19 @@ def download_rootfs(distro_name: str, distro_version: str) -> None:
                 # print_status("Downloading pop-os rootfs from eupnea GitHub releases, part 2/2")
                 # download_file("https://github.com/eupnea-linux/pop-os-rootfs/releases/latest/download/pop-os-rootfs"
                 #              "-22.04.split.ab", "/tmp/depthboot-build/pop-os-rootfs.split.ab")
+                print_status("Downloading sha256sum for pop-os rootfs")
+                download_file(
+                    "https://github.com/eupnea-linux/pop-os-rootfs/releases/latest/download/pop-os-rootfs-22.04.sha256",
+                    "/tmp/depthboot-build/checksum.sha256")
                 print_status("Combining split pop-os rootfs, might take a while")
                 bash("cat /tmp/depthboot-build/pop-os-rootfs.split.?? > /tmp/depthboot-build/pop-os-rootfs.tar.xz")
     except URLError:
         print_error("Couldn't download rootfs. Check your internet connection and try again. If the error persists, "
                     "create an issue with the distro and version in the name")
         sys.exit(1)
+    # verify rootfs sha256sum
+    print_status("Verifying rootfs sha256sum")
+    bash("cd /tmp/depthboot-build && sha256sum --ignore-missing --check /tmp/depthboot-build/checksum.sha256")
 
 
 # Create, mount, partition the img and flash the eupnea kernel
